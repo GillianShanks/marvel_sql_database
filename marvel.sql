b211965cda99a659ec12f2115467a573739d6559
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-29 14:41:43 +0100

    Adds commented out answers to MVP questions - using SELECT, UPDATE, DELETE and INSERT.

--- marvel.sql
+++ marvel.sql
@@ -49,3 +49,25 @@ INSERT INTO movies (title, year, show_time) VALUES ('Guardians of the Galaxy 2',
 INSERT INTO movies (title, year, show_time) VALUES ('Spider-Man: Homecoming', 2017, '23:00');
 INSERT INTO movies (title, year, show_time) VALUES ('Thor: Ragnarok', 2017, '22:10');
 INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:00');
+
+-- SELECT * FROM movies;
+
+-- SELECT name FROM people;
+
+-- UPDATE people SET name = 'Krusty the Clown' WHERE id = 13;
+
+-- SELECT name FROM people WHERE id = 1;
+
+-- DELETE FROM movies WHERE id = 9;
+
+-- INSERT INTO people (name) VALUES ('Bart Simpson');
+
+-- DELETE FROM people WHERE id = 11;
+
+-- INSERT INTO movies (title, show_time) VALUES ('Avengers: Infinity War', '00:00');
+
+-- SELECT show_time FROM movies WHERE title = 'Iron Man 2';
+-- UPDATE movies SET show_time = '20:45' WHERE id = 7;
+
+-- SELECT * FROM movies;
+-- SELECT * FROM people;
